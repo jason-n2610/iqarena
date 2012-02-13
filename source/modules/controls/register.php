8febfcbbca7e9acdad85dd381b7e3e1c2ee7028a
hoangnh29@gmail.com <hoangnh29@gmail.com@9a05e8d4-5491-a5d3-fd01-7b981a2bfdb5>
2012-02-13 04:08:18 +0000

    su dung cac phuong thuc static -> toi uu code

--- source/modules/controls/register.php
+++ source/modules/controls/register.php
@@ -6,40 +6,35 @@
     require ('/include/mysql.php');
     require ('/modules/models/user.php');
 
-    // tao doi tuong mysql va user
-    $objUser = new user();
-    $objMySql = new mysql();
-
     // connect database
-    $objMySql->connect();
+    MySQL::connect();
 
-    $result = $objUser->getAllUser();
+    $result = User::getAllUser();
     // lay ve thong tin tat ca user
     while ($row = mysql_fetch_array($result))
     {
         echo $row['username'] . " " . $row['passwd'] . " " . $row['registed_date'];
         echo "<br />";
     }
-
-    $objUser->freeResult();
-
+    
     // lay ve user admin
-    $result = $objUser->getUserByUserName('admin2');
+    $result = User::getUserByUserName('admin1');
     if (mysql_num_rows($result) != 0)
     {
-        echo '!= 0 <br/>';
         while ($row = mysql_fetch_array($result))
         {
             echo "get pass user: admin " . $row['user_id'];
         }
     } else
     {
-        echo '== 0 <br/>';
         echo "ko ton tai user";
     }
+    
+    // giai phong du lieu    
+    unset($result);
 
     // dong ket noi
-    $objMySql->close();
+    MySQL::close();
 
     // }
 
